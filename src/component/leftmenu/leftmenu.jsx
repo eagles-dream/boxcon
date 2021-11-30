3b96370c46a059cc540ae27e9c736322df4a8c6d
eagles70 <jkkim70@gmail.com>
2021-11-30 22:14:21 +0900

    mobile menu UI

--- src/component/leftmenu/leftmenu.jsx
+++ src/component/leftmenu/leftmenu.jsx
@@ -5,24 +5,19 @@ import { Link } from 'react-router-dom';
 const LeftMenu = ({open, toggle}) => {
   return(
     <div className={!open ? styles.container_close : styles.container}>
-      <Link to='home' className={styles.menu_home} onClick={toggle}>
-        <span className={styles.icon}><i className="fas fa-home"></i></span>
+      <Link to='home' className={styles.menu} onClick={toggle}>
         <span className={styles.text}>홈</span>
       </Link>
       <Link to='product' className={styles.menu} onClick={toggle}>
-        <span className={styles.icon}><i className="fas fa-boxes"></i></span>
         <span className={styles.text}>상품관리</span>
       </Link>
       <Link to='stock' className={styles.menu} onClick={toggle}>
-        <span className={styles.icon}><i className="fas fa-pallet"></i></span>
         <span className={styles.text}>재고관리</span>
       </Link>
       <Link to='order' className={styles.menu} onClick={toggle}>
-        <span className={styles.icon}><i className="fas fa-cart-arrow-down"></i></span>
         <span className={styles.text}>주문관리</span>
       </Link>
-      <Link to='setting' className={styles.menu_setting} onClick={toggle}>
-        <span className={styles.icon}><i className="fas fa-cogs"></i></span>
+      <Link to='setting' className={styles.menu} onClick={toggle}>
         <span className={styles.text}>설정</span>
       </Link>
     </div>
